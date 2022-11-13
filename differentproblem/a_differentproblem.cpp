0501dff58c4e6d6aa6ff97fdacef5cf37212c5bc
natheDev <tuanjunior98@gmail.com>
2022-11-13 09:14:40 +0700

    accepted

---
+++ differentproblem/a_differentproblem.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+    long long int a, b;
+    while (cin >> a, cin >> b)
+    {
+        cout << abs(a - b) << endl;
+    }
+
+    return 0;
+}
